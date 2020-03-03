b15a1a03fb674591540accd0b09daaf0a818aa3b
Thai Pangsakulyanont <dtinth@spacet.me>
2020-03-03 18:05:12 +0700

    Add fake store switching

--- src/fake-redux-store/index.js
+++ src/fake-redux-store/index.js
@@ -7,3 +7,7 @@ export const fakeReduxStore = createStore(() => FAKE_STATE);
 export function isFakeState(state) {
   return state.__FAKE_REDUX_STORE__;
 }
+
+export function provideFakeSelectorResult(selector, fakeResult) {
+  return state => (isFakeState(state) ? fakeResult : selector(state));
+}
